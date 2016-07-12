6eddf85a0d774559b4f0a0abd4a98a378d2bc5df
Jacob Evans <jacob@dekz.net>
2016-07-12 23:07:17 +1000

    Update and check

--- plugins.sh
+++ plugins.sh
@@ -45,3 +45,80 @@ function uninstall {
     fi
   fi
 }
+
+function update_plugin {
+  local plugin_dir=$1
+  cd $1
+  git pull origin master 2>/dev/null && echo "Updated $plugin_dir"
+}
+
+function check_plugin {
+  local dir=$1
+  cd $dir
+  local local_rev=$(git rev-parse @)
+  local remove_rev=$(git rev-parse @{u})
+  local base_rev=$(git merge-base @ @{u})
+
+  local base=$(basename `pwd`)
+  if [ $local_rev = $remove_rev ]; then
+      echo "$base Up to date"
+  elif [ $local_rev = $base_rev ]; then
+      echo "$base Update available"
+  elif [ $remove_rev = $base_rev ]; then
+      echo "$base Need to push"
+  else
+      echo "$base Diverged"
+  fi
+}
+
+function check {
+  if [ -z "${1-}" ]; then
+    echo "Error: Unsupported option"
+    exit 1
+  fi
+
+  local plugin=$1
+  if [ "$plugin" == "--all" ]; then
+    echo "Checking all"
+    cd "$AWSM_PLUGINS_DIR"
+    for f in $AWSM_PLUGINS_DIR/*; do
+      if [ -d "$f" ]; then
+        check_plugin $f
+      fi
+    done
+  else
+    local plugin_dir="$AWSM_PLUGINS_DIR/$plugin"
+    if [ -d "$plugin_dir" ]; then
+      cd "$plugin_dir"
+      check_plugin "$plugin_dir"
+    else
+      echo "Error: no such plugin $plugin"
+    fi
+  fi
+}
+
+function update {
+  if [ -z "${1-}" ]; then
+    echo "Error: Unsupported option"
+    exit 1
+  fi
+
+  local plugin=$1
+  if [ "$plugin" == "--all" ]; then
+    echo "Updating all"
+    cd "$AWSM_PLUGINS_DIR"
+    for f in $AWSM_PLUGINS_DIR/*; do
+      if [ -d "$f" ]; then
+        update_plugin $f
+      fi
+    done
+  else
+    local plugin_dir="$AWSM_PLUGINS_DIR/$plugin"
+    if [ -d "$plugin_dir" ]; then
+      cd "$plugin_dir"
+      update_plugin "$plugin_dir"
+    else
+      echo "Error: no such plugin $plugin"
+    fi
+  fi
+}
